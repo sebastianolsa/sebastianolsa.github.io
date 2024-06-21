44ac346fe0c3d7f4bbd0d831957b23423e25e10e
sebastiano <xBest25x@gmail.com>
2024-06-21 16:53:46 -0700

    added and animated about button

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const track = document.getElementById("track")
+const aboutBtn = document.getElementById("aboutBtn")
+
+let mouseDownX;
+let mouseDownY;
 
 const mapNumRange = (num, inMin, inMax, outMin, outMax) =>
   ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 
+//window setup / track controls
 
 window.onload = e => {
     for (let i = 0; i < track.childElementCount; i++) {
@@ -10,22 +15,23 @@ window.onload = e => {
 
         if (image.nodeName === 'IMG') {
             const id = parseInt(image.id[3])
-            let image_slide = mapNumRange(0, id * 100 / track.childElementCount, (id+1) * 100 / track.childElementCount, 42, 58);
+            let image_slide = mapNumRange(0, id * 100 / track.childElementCount, (id+1) * 100 / track.childElementCount, 46, 54);
             image.style.objectPosition = `${image_slide}% 50%`
         }
     }
 }
 
-
 window.onmousedown = e => {
     track.dataset.mouseDownAt = e.clientX;
+    mouseDownX = e.clientX;
+    mouseDownY = e.clientY;
 }
 
 window.onmousemove = e => {
     if (track.dataset.mouseDownAt === "0") return;
 
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
-    const maxDelta = window.innerWidth;
+    const maxDelta = window.innerWidth * 1.5;
     const percentage = (mouseDelta / maxDelta) * 100;
 
     const nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
@@ -43,8 +49,7 @@ window.onmousemove = e => {
 
         if (image.nodeName === 'IMG') {
             const id = parseInt(image.id[3]);
-            let image_slide = mapNumRange(nextPercentageConst, id * 100 / item_count, (id+1) * 100 / item_count, 42, 58);
-
+            const image_slide = mapNumRange(nextPercentageConst, id * 100 / item_count, (id+1) * 100 / item_count, 46, 54);
 
             image.animate({
                 objectPosition: `${image_slide}% 50%`
@@ -56,4 +61,11 @@ window.onmousemove = e => {
 window.onmouseup = e => {
     track.dataset.mouseDownAt = "0";
     track.dataset.prevPercentage = track.dataset.percentage;
+
+}
+
+//about button controls
+
+aboutBtn.onclick = e => {
+    console.log("about btn pressed");
 }
