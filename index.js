23fd09c8152ca1bd24ba7a009af33d360d288887
sebastiano <xBest25x@gmail.com>
2024-07-22 15:43:20 +0200

    easing animations

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const aboutBtn = document.getElementById("aboutBtn")
 let mouseDownX;
 let mouseDownY;
 let inAbout = false;
+let click = false;
 
 const mapNumRange = (num, inMin, inMax, outMin, outMax) =>
   ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+
+const distTwoPoints = (x1, y1, x2, y2) =>
+    Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+
 
 //window setup / track controls
 
@@ -50,17 +55,30 @@ window.onmousemove = e => {
         const image = track.children[i];
 
         if (image.nodeName === 'IMG') {
-            const id = parseInt(image.id[3]);
+            let id;
+            if (image.id.length === 5) {
+                id = parseInt(image.id[3]+image.id[4]); //horrible solution for images with two digit id's, i know, im sorry
+            } else {
+                id = parseInt(image.id[3]);
+            }
             const image_slide = mapNumRange(nextPercentageConst, id * 100 / item_count, (id+1) * 100 / item_count, 46, 54);
 
             image.animate({
                 objectPosition: `${image_slide}% 50%`
-            }, { duration: 1500, fill: "forwards" } )
+            }, { duration: 1500, fill: "forwards", easing: "ease-in-out" } )
         }
     }
 }
 
 window.onmouseup = e => {
+    if (distTwoPoints(mouseDownX, mouseDownY, e.clientX, e.clientY) < 10) {
+        for (let i = 0; i < track.childElementCount; i++) {
+            let center = (100/(track.childElementCount-1))*i
+            if (Math.abs(track.dataset.percentage - center) < (44/(track.childElementCount-1))+0.23) {
+                console.log(i) //the image over which the user is hovering
+            }
+        }
+    }
     track.dataset.mouseDownAt = "0"; //set back to default meaning mouse hasnt been pressed
     track.dataset.prevPercentage = track.dataset.percentage; //update the scroll percentage
 
@@ -73,19 +91,19 @@ aboutBtn.onclick = e => {
 
     //flip and move button
     aboutBtn.animate({
-        transform: `scaleX(175%) scaleY(${50 * (inAbout ? 1 : -1)}%)`
-    }, { duration: 1000, fill: "forwards" })
+        transform: `scaleX(200%) scaleY(${50 * (inAbout ? 1 : -1)}%)`
+    }, { duration: 1000, fill: "forwards", easing: "ease-in-out"  })
 
     document.getElementById('nav').animate({
         bottom: `${inAbout ? 85 : -0}%`
-    }, { duration: 1000, fill: "forwards" })
+    }, { duration: 1000, fill: "forwards", easing: "ease-in-out" })
 
     //move track and selector up and off screen too
     track.animate({
         top: `${inAbout ? -35 : 50}%`
-    }, { duration: 1000, fill: "forwards" })
+    }, { duration: 1000, fill: "forwards", easing: "ease-in-out"  })
 
     document.getElementById('selector').animate({
         top: `${inAbout ? -35 : 50}%`
-    }, { duration: 1000, fill: "forwards" })
+    }, { duration: 1000, fill: "forwards", easing: "ease-in-out"  })
 }
